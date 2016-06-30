e5cdd322cc549f5bbace6171bb821929d7d23c5d
zsmackpint <pintozachary@yahoo.com>
2016-06-30 09:14:20 -0700

    Update PythonChallenge1.py

--- PythonChallenge1.py
+++ PythonChallenge1.py
@@ -1,5 +1,6 @@
 string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 new = ""
+
 for i in range(0, len(string)):
     if ord(string[i]) >= 97 and ord(string[i]) <= 122:
         if(ord(string[i])+2) == 123:
@@ -18,6 +19,7 @@ print(new)
 
 string2 = "map"
 new2 = ""
+
 for i in range(0, len(string2)):
     if ord(string2[i]) >= 97 and ord(string2[i]) <= 122:
         if(ord(string2[i])+2) == 123:
