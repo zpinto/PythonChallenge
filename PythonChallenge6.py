9e4f597f28e9aef20183f62eb678e975ca78b4c0
zsmackpint <pintozachary@yahoo.com>
2016-06-30 09:13:23 -0700

    Update PythonChallenge6.py

--- PythonChallenge6.py
+++ PythonChallenge6.py
@@ -2,17 +2,16 @@
 import zipfile
 from zipfile import ZipFile
 
-
 ext = ".txt"
 num = "90052"
 inf = []
 res = ""
 string = ""
+
 with ZipFile("channel.zip", "r") as myzip:
         inf = myzip.infolist()
-for i in range (0, 910):
-    
 
+for i in range (0, 910):
     for item in inf:
         if (num + ext) == item.filename:
             if str(item.comment)[2] == "\\":
@@ -24,6 +23,7 @@ for i in range (0, 910):
     file = open((num + ext), "rb")
     res = file.read()
     num = ""
+   
     for n in range(0, len(res)):
         if 57 >= res[n] >= 48:
             num += chr(res[n])
